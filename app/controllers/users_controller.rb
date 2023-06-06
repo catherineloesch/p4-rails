4770a41910f0dc7eb471736c2e3f7a31376663bf
Catherine Loesch <catherine.loesch@protonmail.com>
2023-06-06 12:18:49 +0100

    update show route for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
     def index
         @users = User.all
         render json: @users
