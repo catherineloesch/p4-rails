b3e27c39442be7de70710ab4dd5fb96ff80d6215
Catherine Loesch <catherine.loesch@protonmail.com>
2023-06-08 04:45:37 +0100

    trying to solve netlify deployment issue with cors

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,11 @@ module P4Rails
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
     config.middleware.use ActionDispatch::Flash
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
