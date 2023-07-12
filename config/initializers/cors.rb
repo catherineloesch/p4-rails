d8e53b88e7396887eb77e4209f5f1317f6982813
Catherine Loesch <catherine.loesch@protonmail.com>
2023-07-12 21:37:10 +0100

    fixing netlify issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
   allow do
-    origins 'https://pawneepets.netlify.app', 'http://localhost:3000'
+    origins '*'
     resource(
       '*',
       headers: :any,
